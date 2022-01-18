a1b09acc250ad7ba8129d7cb3ca74dbf67dc2544
David Jenni <me@davidjenni.ch>
2022-01-18 16:42:29 +0000

    src/Indexer: add classes to represent documents and definitions

---
+++ src/Indexer/Definition.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LsifPhp\Indexer;
+
+/** Definition represents an LSIF definition. */
+final class Definition
+{
+
+    /** @var array<int, int[]> */
+    private array $referenceRangeIds;
+
+    public function __construct(
+        private int $docId,
+        private int $rangeId,
+        private int $resultSetId,
+    ) {
+        $this->referenceRangeIds = [];
+    }
+
+    public function docId(): int
+    {
+        return $this->docId;
+    }
+
+    public function rangeId(): int
+    {
+        return $this->rangeId;
+    }
+
+    public function resultSetId(): int
+    {
+        return $this->resultSetId;
+    }
+
+    public function addReferenceRangeId(int $docId, int $rangeId): void
+    {
+        if (!isset($this->referenceRangeIds[$docId])) {
+            $this->referenceRangeIds[$docId] = [];
+        }
+        $this->referenceRangeIds[$docId][] = $rangeId;
+    }
+
+    /** @return array<int, int[]> */
+    public function referenceRangeIds(): array
+    {
+        return $this->referenceRangeIds;
+    }
+}
---
+++ src/Indexer/Document.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LsifPhp\Indexer;
+
+use PhpParser\Node\Stmt;
+
+/** Document represents an LSIF document. */
+final class Document
+{
+
+    /** @var int[] */
+    private array $definitionRangeIds;
+
+    /** @var int[] */
+    private array $referenceRangeIds;
+
+    /** @param  Stmt[]  $stmts */
+    public function __construct(
+        private int $id,
+        private string $path,
+        private string $code,
+        private array $stmts,
+    ) {
+        $this->definitionRangeIds = [];
+        $this->referenceRangeIds = [];
+    }
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function path(): string
+    {
+        return $this->path;
+    }
+
+    public function code(): string
+    {
+        return $this->code;
+    }
+
+    /** @return Stmt[] */
+    public function stmts(): array
+    {
+        return $this->stmts;
+    }
+
+    public function addDefinitionRangeId(int $rangeId): void
+    {
+        $this->definitionRangeIds[] = $rangeId;
+    }
+
+    /** @return int[] */
+    public function definitionRangeIds(): array
+    {
+        return $this->definitionRangeIds;
+    }
+
+    /** @return int[] */
+    public function referenceRangeIds(): array
+    {
+        return $this->referenceRangeIds;
+    }
+
+    public function addReferenceRangeId(int $rangeId): void
+    {
+        $this->referenceRangeIds[] = $rangeId;
+    }
+}
